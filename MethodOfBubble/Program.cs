3c1d718bd88587c74b8d0b3f1cc388228529519e
T-Midnight <BelousovaTanya1995@yandex.ru>
2013-11-14 12:09:56 +0400

    project upload

--- MethodOfBubble/Program.cs
+++ MethodOfBubble/Program.cs
@@ -12,7 +12,6 @@ namespace MethodOfBubble
             Console.WriteLine("Введите количество элементов массива");
             int size = Int32.Parse(Console.ReadLine());
             int[] m1 = new int[size];
-            int[] m2 = new int[size];
 
             int i, j;
 
@@ -23,17 +22,16 @@ namespace MethodOfBubble
 
             for (i = 0; i < m1.Length; i++) {
                 for (j = 0; j < (m1.Length - 1) ; j++) {
-                    m2[i] = m1[i];
-                    if (m2[j] > m2[j + 1]) {
-                        int a = m2[j];
-                        m2[j] = m2[j + 1];
-                        m2[j + 1] = a;
+                    if (m1[j] > m1[j + 1]) {
+                        int a = m1[j];
+                        m1[j] = m1[j + 1];
+                        m1[j + 1] = a;
                     }
                 }
             }
 
-            for (i = 0; i < m2.Length; i++) {
-                Console.Write(m2[i] + " ");
+            for (i = 0; i < m1.Length; i++) {
+                Console.Write(m1[i] + " ");
             }
 
                 Console.ReadKey();
