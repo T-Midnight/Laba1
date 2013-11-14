3667cbcece05c6c3964dfe8dafafdac2bba38286
T-Midnight <BelousovaTanya1995@yandex.ru>
2013-11-14 17:34:56 +0400

    +

--- MethodOfBubble/Program.cs
+++ MethodOfBubble/Program.cs
@@ -1,44 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-// сколько библиотек ты реально используешь
 
 namespace MethodOfBubble
 {
-    class Program
-    {
-        // string[] args пробовала удалять? ты же им не пользуешься.
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main() {
             Console.WriteLine("Введите количество элементов массива");
             int size = Int32.Parse(Console.ReadLine());
-            // массив называется m1, почему?
-            int[] m1 = new int[size];
-            //индексы надо объявлять внутри каждого цикла, а не перед всеми
-            int i, j;
+            int[] Array = new int[size];
 
-            for (i = 0; i < m1.Length; i++) {
+            for (int i = 0; i < Array.Length; i++) {
                 Console.WriteLine("Введите  " + i + "  элемент массива");
-                m1[i] = Int32.Parse(Console.ReadLine());
+                Array[i] = Int32.Parse(Console.ReadLine());
             }
 
-            for (i = 0; i < m1.Length; i++) {
-                for (j = 0; j < (m1.Length - 1) ; j++) {
-                    if (m1[j] > m1[j + 1]) {
-                        int a = m1[j];
-                        m1[j] = m1[j + 1];
-                        m1[j + 1] = a;
+            for (int i = 0; i < Array.Length; i++) {
+                for (int j = 0; j < (Array.Length - 1) ; j++) {
+                    if (Array[j] > Array[j + 1]) {
+                        int a = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = a;
                     }
                 }
             }
 
-            for (i = 0; i < m1.Length; i++) {
-                Console.Write(m1[i] + " ");
+            for (int i = 0; i < Array.Length; i++) {
+                Console.Write(Array[i] + " ");
             }
-            // неформатированный код :)
 
-                Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
